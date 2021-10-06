eeafca4111a1c0f9a4e7f084c5aa7473d84b5827
rahulsanjay01 <90293567+rahulsanjay01@users.noreply.github.com>
2021-10-06 00:42:28 -0700

    added the functionality of viewing even numbers in bold

--- js/site.js
+++ js/site.js
@@ -45,8 +45,17 @@ function displayNumbers(numbers){
 let templateRows = "";
     for (let index = 0; index < numbers.length; index++) {
         
+        let className = "even";
         let number = numbers[index];
-        templateRows += `<tr><td>${number}</td></tr>`;
+
+        if(number % 2 === 0) {
+            className = "even";
+        }
+        else{
+            className = "odd";    
+        }
+
+        templateRows += `<tr><td class="${className}">${number}</td></tr>`;
         
     }
 
